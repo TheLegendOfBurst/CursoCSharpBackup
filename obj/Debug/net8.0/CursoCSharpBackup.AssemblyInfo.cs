fed18e8a1b28b7f28206da9fad8401821d365df7
Burst <141189394+TheLegendOfBurst@users.noreply.github.com>
2024-08-26 17:43:58 -0300

    12

--- obj/Debug/net8.0/CursoCSharpBackup.AssemblyInfo.cs
+++ obj/Debug/net8.0/CursoCSharpBackup.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoCSharpBackup")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e97d187e012f68f321bd8592b3969d32f08a2e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da739020d850e3c387d8f0f4d58db235b4583428")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoCSharpBackup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoCSharpBackup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
